9fb029d6ddce7ed9dedb964122fcd71d0eb7ed79
a2361253285 <2361253285@qq.com>
2019-04-21 14:17:12 +0800

    信号驱动IO

---
+++ unit25_signal_driver_io/dgecho_signal.c
@@ -0,0 +1,105 @@
+#include "../myunp.h"
+
+static int sockfd;
+
+#define QSIZE 8
+#define MAXDG 4096
+
+typedef struct {
+    void *dg_data;
+    size_t dg_len;
+    struct sockaddr *dg_sa;
+    socklen_t dg_salen;
+} DG;
+
+static DG dg[QSIZE];
+static long cntread[QSIZE+1];
+
+static int iget;
+static int iput;
+static int nqueue;
+static socklen_t clilen;
+
+static void sig_io(int);
+static void sig_hup(int);
+
+
+void dg_echo(int sockfd_arg, SA *pcliaddr, socklen_t clilen_arg){
+    int i;
+    const int on = 1;
+    sigset_t zeromask, newmask, oldmask;
+
+    sockfd = sockfd_arg;
+    clilen = clilen_arg;
+
+    for (i = 0; i < QSIZE; i++){
+        dg[i].dg_data = malloc(MAXDG);
+        dg[i].dg_sa = malloc(clilen);
+        dg[i].dg_salen = clilen;
+    }
+    iget = iput = nqueue = 0;
+    signal(SIGHUP, sig_hup);
+    signal(SIGIO, sig_io);
+    Fcntl(sockfd, __F_SETOWN, getpid());
+    Ioctl(sockfd, FIOASYNC, &on);
+    Icotl(sockfd, FIONBIO, &on);
+
+    Sigemptyset(&zeromask);
+    Sigemptyset(&oldmask);
+    Sigemptyset(&newmask);
+    Sigaddset(&newmask, SIGIO);
+
+    Sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+    while (1){
+        while (nqueue = 0)
+            sigsuspend(&zeromask);
+        
+        Sigprocmask(SIG_SETMASK, &oldmask, NULL);
+
+        sendto(sockfd, dg[iget].dg_data, dg[iget].dg_len, 0, dg[iget].dg_sa, dg[iget].dg_salen);
+        if (++iget >= QSIZE)
+            iget = 0;
+
+        Sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+        nqueue -- ;
+
+    }
+
+}
+
+
+static void sig_io(int signo){
+    ssize_t len;
+    int nread;
+    DG *ptr;
+
+    for (nread = 0; ; ){
+        if (nqueue >=  QSIZE)
+            err_quit("receive overflow");
+        ptr = &dg[iput];
+        ptr->dg_salen = clilen;
+        len = recvfrom(sockfd, ptr->dg_data, MAXDG, 0, ptr->dg_sa, &ptr->dg_salen);
+
+        if (len < 0){
+            if (errno == EWOULDBLOCK)
+                break;
+            else
+                err_sys("recvfrom error");            
+        }
+        ptr->dg_len = len;
+
+        nread++;
+        nqueue++;
+        if (++iput >= QSIZE)
+            iput = 0; 
+    }
+    cntread[nread] ++;
+}
+
+
+static void sig_hup(int signo){
+    for (int i = 0; i <= QSIZE; i++){
+        printf("cntread[%d] = %ld\n", i, cntread[i]);
+        
+    }
+}
